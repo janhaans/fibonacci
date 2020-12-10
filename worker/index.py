e8bedac460362e9f7f4e07247f33368cd0dbe2a5
Jan Haans <jan.haans@gmail.com>
2020-12-10 14:51:58 +0100

    Added worker

---
+++ worker/index.py
@@ -0,0 +1,19 @@
+import redis
+import os
+
+def fib(num):
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    return fib(num-1) + fib(num-2)
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+subscription = redis_client.pubsub()
+subscription.subscribe('message')
+
+for new_message in subscription.listen():
+    fib(new_message)
